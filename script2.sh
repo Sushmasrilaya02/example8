b487643a1d9436b38c08a89b8f2efc5320599bad
Sushmasrilaya02 <61860567+Sushmasrilaya02@users.noreply.github.com>
2024-06-24 22:19:51 +0530

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,3 @@
+echo $user
+variable_name="this script is written to understand the variables"
+echo $variable_name
---
+++ script4.sh
@@ -0,0 +1,7 @@
+x=12
+y=10
+if [ $x -ne $y ]
+then
+     echo " Both values are not equal"
+fi
+echo " if statement ended "
